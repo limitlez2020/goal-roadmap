bb89becbfebda87eaa51b97b5b69c735b0f2ff42
fortune.akinremi <fortuneakinremi@gmail.com>
2025-05-05 00:12:09 -0400

    Added bento grid to bottom of the landing home page

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
 
       {/* Navbar */}
       <div className="flex flex-row w-full p-4 justify-between items-center gap-5 border-b border-neutral-500 mb-24">
-        <div className={`flex text-center text-sm`}>Goal Quest</div>
+        <div className={`flex text-center text-base ${press_start_2p.className}`}>G.</div>
         <button className="flex p-1 px-3 items-center justify-center text-center bg-[#FF77B2] text-neutral-900 font-bold text-sm cursor-pointer">login</button>
       </div>
       
@@ -40,15 +40,17 @@ export default function Home() {
             </button>
           </div>
 
+
           {/* How it works */}
           <div className="flex flex-col w-9/10 justify-start items-start mb-20">
-            <p className="uppercase text-xl font-medium mb-7">How it works:</p>
+            <p className="text-xl font-medium">How it works.</p>
+            <p className="text-sm mb-7 text-neutral-400">Here is how to level up your life</p>
             {/* Top */}
-            <div className="flex flex-row w-full">
-              {/* <div className="w-2/3 h-80 overflow-hidden border-dashed border">
-                <Image src={"/placeholder.jpg"} alt="" width={1980} height={1080} className="w-[100%] h-[100%] object-cover"/>
-              </div> */}
-              <p className={`flex text-6xl [word-spacing:-70px] ${press_start_2p.className}`}>1 .</p>
+            <div className="flex flex-row grid grid-cols-3 gap-3 w-full h-150 justify-center">
+              <div className="flex row-span-1 col-span-3 w-full border bg-[#FDEF64]">Grid 1</div>
+              <div className="flex row-span-1 col-span-1 w-full border bg-[#32EE7C]">Grid 2</div>
+              <div className="flex row-span-1 col-span-2 w-full border bg-[#C734F1]">Grid 3</div>
+              <div className="flex row-span-1 col-span-3 w-full border bg-[#F49F5E]">Grid 4</div>
             </div>
           </div>
         </div>
